1ec66ae2a519530b7538d7871373e49589b1b9c9
ByAgus314 <agustincollaogonzalez@gmail.com>
2022-06-19 12:04:19 -0400

    actualizacion

--- comandos/admin-warns-see.js
+++ comandos/admin-warns-see.js
@@ -9,9 +9,9 @@ module.exports = {
 
 execute (client, message, args){
 
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permissions to use this command.')
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('No tienes los permisos suficientes')
     const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-    if(!user) return message.channel.send('User not found.')
+    if(!user) return message.channel.send('Usuario no encontrado')
     const reason = args.slice(1).join(" ")
     db.findOne({ guildid: message.guild.id, user: user.user.id}, async(err, data) => {
         if(err) throw err;
